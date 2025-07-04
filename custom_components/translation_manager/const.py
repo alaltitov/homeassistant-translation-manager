4cfa0aa5ec6adf0980f81edfded61cdf56908e46
Alexey Titov <137920178+alaltitov@users.noreply.github.com>
2025-07-04 22:17:19 +0300

    Init

---
+++ custom_components/translation_manager/const.py
@@ -0,0 +1,6 @@
+"""Constants for the Translation Manager integration."""
+
+DOMAIN = "translation_manager"
+STORAGE_KEY = f"{DOMAIN}.storage"
+STORAGE_VERSION = 1
+SENSOR_ENTITY_ID = "sensor.translation_manager"
