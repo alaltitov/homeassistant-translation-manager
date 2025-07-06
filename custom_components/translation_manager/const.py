33208b0b23e7e780584da45079feb683c9e66da3
Alexey Titov <137920178+alaltitov@users.noreply.github.com>
2025-07-06 18:40:36 +0300

    Delete custom_components directory

--- custom_components/translation_manager/const.py
+++
@@ -1,6 +0,0 @@
-"""Constants for the Translation Manager integration."""
-
-DOMAIN = "translation_manager"
-STORAGE_KEY = f"{DOMAIN}.storage"
-STORAGE_VERSION = 1
-SENSOR_ENTITY_ID = "sensor.translation_manager"
